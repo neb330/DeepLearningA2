2d26617b38849eec2ed62a29d687fcf70f4f364b
neb330@nyu.edu <neb330@nyu.edu>
2017-03-27 21:45:36 -0400

    modifying data helpers function

--- data_helpers.py
+++ data_helpers.py
@@ -5,6 +5,7 @@ import collections
 import os
 import nltk
 import codecs
+import io
 
 
 
@@ -12,14 +13,14 @@ def prepare_data(path, vocabsize):
     count = [['<unk>', -1]]
     outdir = "data/gutenberg_clean/"
     # Load data from files
-    with open((os.path.join(path, "train.txt")), "r",  encoding = "ISO-8859-1") as f:
+    with io.open((os.path.join(path, "train.txt")), "r",  encoding = "ISO-8859-1") as f:
         words = [word for line in f for word in line.split()]
         count.extend(collections.Counter(words).most_common(vocabsize - 1))
         count = dict(count)
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     for filename in os.listdir(path):
-        text = open((os.path.join(path, filename)), "r",  encoding = "ISO-8859-1").readlines()
+        text = io.open((os.path.join(path, filename)), "r",  encoding = "ISO-8859-1").readlines()
         outfile = codecs.open((outdir + filename), "w", "utf-8")
         for line in text:
             lst = line.split()
